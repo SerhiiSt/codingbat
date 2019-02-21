f2223c2e1124a87db1728622d59b7d1b350d74f5
Sergio <shtelmah.serhiy@gmail.com>
2019-02-21 21:13:54 +0100

    add new package and new  files

---
+++ src/main/java/array_2/CountEvens.java
@@ -0,0 +1,27 @@
+package array_2;
+
+/**
+ * Created by Sergio on 2/21/19.
+ */
+
+
+/**
+ * Return the number of even ints in the given array. Note: the % "mod" operator computes the remainder, e.g. 5 % 2 is 1.
+
+ countEvens([2, 1, 2, 3, 4]) → 3
+ countEvens([2, 2, 0]) → 3
+ countEvens([1, 3, 5]) → 0
+ */
+public class CountEvens {
+
+    public int countEvens(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
