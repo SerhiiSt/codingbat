b70eaa0f2bf25a10d466da24268b32f6dfb7f28b
Sergio <shtelmah.serhiy@gmail.com>
2019-03-03 20:49:58 +0100

    format file

--- src/main/java/array_2/NotAlone.java
+++ src/main/java/array_2/NotAlone.java
@@ -21,20 +21,18 @@ import java.util.Arrays;
 public class NotAlone {
 
     public int[] notAlone(int[] nums, int val) {
-        for (int i=1;i<nums.length-1;i++){
-            if (nums[i]==val){
-                if (nums[i-1]!=nums[i] && nums[i+1]!=nums[i]){
-                    if (nums[i+1]>nums[i-1]){
-                        nums[i]=nums[i+1];
-                    }
-                    else if (nums[i+1]<nums[i-1]){
-                        nums[i]= nums[i-1];
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (nums[i] == val) {
+                if (nums[i - 1] != nums[i] && nums[i + 1] != nums[i]) {
+                    if (nums[i + 1] > nums[i - 1]) {
+                        nums[i] = nums[i + 1];
+                    } else if (nums[i + 1] < nums[i - 1]) {
+                        nums[i] = nums[i - 1];
                     }
                 }
             }
         }
         return nums;
-
 
 
     }
