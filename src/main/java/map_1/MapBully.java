666083cfe2c59af09658cd2acf958c639a313f62
Sergio <shtelmah.serhiy@gmail.com>
2020-01-11 23:00:46 +0100

    add map solutions

---
+++ src/main/java/map_1/MapBully.java
@@ -0,0 +1,27 @@
+package map_1;
+
+/**
+ * Created by Sergio on 11/9/19.
+ */
+
+import java.util.Map;
+
+/**
+ * Modify and return the given map as follows: if the key "a" has a value,
+ * set the key "b" to have that value, and set the key "a" to have the value "".
+ * Basically "b" is a bully, taking the value and replacing it with the empty string.
+ * <p>
+ * mapBully({"a": "candy", "b": "dirt"}) → {"a": "", "b": "candy"}
+ * mapBully({"a": "candy"}) → {"a": "", "b": "candy"}
+ * mapBully({"a": "candy", "b": "carrot", "c": "meh"}) → {"a": "", "b": "candy", "c": "meh"}
+ */
+public class MapBully {
+    public Map<String, String> mapBully(Map<String, String> map) {
+        if (map.containsKey("a")) {
+            map.put("b", map.get("a"));
+            map.put("a", "");
+        }
+        return map;
+
+    }
+}
