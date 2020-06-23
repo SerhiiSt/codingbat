1fe2fa2ebbb9170ca8656ebc9e96ab9a1f4c9557
Sergio <shtelmah.serhiy@gmail.com>
2020-06-23 21:41:46 +0200

    changed WordCount file

--- src/main/java/map_2/WordCount.java
+++ src/main/java/map_2/WordCount.java
@@ -3,12 +3,23 @@ package map_2;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * The classic word-count algorithm: given an array of strings, return a Map<String, Integer>
+ * with a key for each different string,
+ * with the value the number of
+ * times that string appears in the array.
+ * <p>
+ * <p>
+ * wordCount(["a", "b", "a", "c", "b"]) → {"a": 2, "b": 2, "c": 1}
+ * wordCount(["c", "b", "a"]) → {"a": 1, "b": 1, "c": 1}
+ * wordCount(["c", "c", "c", "c"]) → {"c": 4}
+ */
 public class WordCount {
 
-    public static Map<String, Integer> wordCount(String[] text) {
+    public Map<String, Integer> wordCount(String[] strings) {
         Map<String, Integer> words = new HashMap<>();
 
-        for (String s : text) {
+        for (String s : strings) {
             if (!words.containsKey(s)) {
                 words.put(s, 1);
             } else {
@@ -19,8 +30,4 @@ public class WordCount {
         return words;
     }
 
-    public static void main(String[] args) {
-        String[] text = {"a", "text", "rt", "b", "c", "a", "o", "t", "rt"};
-        System.out.println(wordCount(text));
-    }
 }
