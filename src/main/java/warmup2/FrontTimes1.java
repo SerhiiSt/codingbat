b0ce02fa414e4b70c3a785c0f90e29f97d7033a9
Sergio <shtelmah.serhiy@gmail.com>
2020-06-30 00:26:50 +0200

    add new package and new  files

---
+++ src/main/java/warmup2/FrontTimes1.java
@@ -0,0 +1,28 @@
+package warmup2;
+
+import java.util.stream.Stream;
+import static java.util.stream.Collectors.joining;
+
+/**
+ * Given a string and a non-negative int n, we'll say that the front
+ * of the string is the first 3 chars, or whatever is
+ * there if the string is less than length 3.
+ * Return n copies of the front;
+ * <p>
+ * <p>
+ * frontTimes("Chocolate", 2) → "ChoCho"
+ * frontTimes("Chocolate", 3) → "ChoChoCho"
+ * frontTimes("Abc", 3) → "AbcAbcAbc"
+ */
+public class FrontTimes1 {
+    public static String frontTimes(String str, int n) {
+        if (str.length()>=3){
+           return Stream.generate(() -> str.substring(0,3)).limit(n).collect(joining());
+        }
+        return Stream.generate(() -> str).limit(n).collect(joining());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(frontTimes("Ab", 4));
+    }
+}
---
+++ src/main/java/warmup2/FrontTimes2.java
@@ -0,0 +1,27 @@
+package warmup2;
+
+/**
+ * Given a string and a non-negative int n, we'll say that the front
+ * of the string is the first 3 chars, or whatever is
+ * there if the string is less than length 3.
+ * Return n copies of the front;
+ * <p>
+ * <p>
+ * frontTimes("Chocolate", 2) → "ChoCho"
+ * frontTimes("Chocolate", 3) → "ChoChoCho"
+ * frontTimes("Abc", 3) → "AbcAbcAbc"
+ */
+public class FrontTimes2 {
+    public String frontTimes(String str, int n) {
+        int len = 3;
+        if (str.length() < 3) {
+            len = str.length();
+        }
+        String result = "";
+        String text = str.substring(0, len);
+        for (int i = 0; i < n; i++) {
+            result += text;
+        }
+        return result;
+    }
+}
