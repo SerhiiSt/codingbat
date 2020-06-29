d529655ff177f5156541c2ecb9bcabd689d0abf5
Sergio <shtelmah.serhiy@gmail.com>
2020-06-29 20:03:30 +0200

    add new package and new  files

---
+++ src/main/java/warmup2/StringTimes.java
@@ -0,0 +1,20 @@
+package warmup2;
+
+/**
+ * Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+ * <p>
+ * <p>
+ * stringTimes("Hi", 2) → "HiHi"
+ * stringTimes("Hi", 3) → "HiHiHi"
+ * stringTimes("Hi", 1) → "Hi"
+ */
+public class StringTimes {
+    public String stringTimes(String str, int n) {
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            text.append(str);
+        }
+        return text.toString();
+
+    }
+}
