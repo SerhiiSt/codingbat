3cb28b875870250511396e17ff726252e7322e1d
Sergio <shtelmah.serhiy@gmail.com>
2020-06-30 17:27:02 +0200

    add new file

---
+++ src/main/java/functional_1/Copies3.java
@@ -0,0 +1,18 @@
+package functional_1;
+
+import java.util.List;
+
+/**
+ * Given a list of strings, return a list where each string is replaced by 3 copies of the string concatenated together.
+ * <p>
+ * <p>
+ * copies3(["a", "bb", "ccc"]) → ["aaa", "bbbbbb", "ccccccccc"]
+ * copies3(["24", "a", ""]) → ["242424", "aaa", ""]
+ * copies3(["hello", "there"]) → ["hellohellohello", "theretherethere"]
+ */
+public class Copies3 {
+    public List<String> copies3(List<String> strings) {
+        strings.replaceAll(s -> s + s + s);
+        return strings;
+    }
+}
